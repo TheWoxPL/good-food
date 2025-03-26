6b45426aec5e88b9febc969bda9ac88feb65de56
Jakub Bujak <jakbuj3@gmail.com>
2025-03-26 18:33:09 +0100

    feat: add manifest and service worker

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import './index.css';
+import '../index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>
 );
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(
+      (registration) => {
+        console.log(
+          'Service Worker registered with scope:',
+          registration.scope
+        );
+      },
+      (error) => {
+        console.log('Service Worker registration failed:', error);
+      }
+    );
+  });
+}
